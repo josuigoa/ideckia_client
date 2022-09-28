e236664cdc140d29ac082c1b59a303fee81f558c
Josu Igoa <josu.igoa@indaba.lks.es>
2022-09-28 16:52:56 +0200

    embed the icon

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use image::io::Reader as ImageReader;
+use std::io::Cursor;
+
 fn main() {
     tracing_subscriber::fmt::init();
 
@@ -7,24 +10,36 @@ fn main() {
         ..Default::default()
     };
 
-    match image::open("icon.ico") {
-        Ok(icon) => {
-            let icon = icon.to_rgba8();
-            let (icon_width, icon_height) = icon.dimensions();
-            native_options.icon_data = Some(eframe::IconData {
-                rgba: icon.into_raw(),
-                width: icon_width,
-                height: icon_height,
-            });
-        }
-        Err(_) => {
-            println!("Could not load the icon.");
-        }
-    }
+    native_options.icon_data = get_icon_data();
 
     eframe::run_native(
         "ideckia client",
         native_options,
         Box::new(|cc| Box::new(ideckia_client::GUI::new(cc))),
     );
+}
+
+fn get_icon_data() -> Option<eframe::IconData> {
+    let icon_bytes = include_bytes!("../icon.ico");
+    let icon = match ImageReader::new(Cursor::new(icon_bytes)).with_guessed_format() {
+        Ok(r) => match r.decode() {
+            Ok(d) => d,
+            Err(e) => {
+                println!("Error guessing icon format: {}", e);
+                return None;
+            }
+        },
+        Err(e) => {
+            println!("Error decoding icon data: {}", e);
+            return None;
+        }
+    };
+
+    let icon = icon.to_rgba8();
+    let (icon_width, icon_height) = icon.dimensions();
+    Some(eframe::IconData {
+        rgba: icon.into_raw(),
+        width: icon_width,
+        height: icon_height,
+    })
 }
